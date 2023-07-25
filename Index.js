8dff29121491198265788c143a708ef70863dcf6
tarunpoddar <tarun.tarun05@gmail.com>
2023-07-25 20:15:33 +0530

    Added Skills, Expereince, Education and Contact section.

--- Index.js
+++ Index.js
@@ -10,6 +10,12 @@ var typed = new Typed(mySpan, props);
 // Check if an element is in the view.
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
+  var res =  rect.top >= 0 &&
+  rect.left >= 0 &&
+  rect.bottom <=
+    (window.innerHeight || document.documentElement.clientHeight) &&
+  rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  console.log(res);
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
@@ -20,10 +26,12 @@ function isInViewport(element) {
 }
 
 var isVisible = false;
+const box = document.querySelector("#myspan");
 
 // Subscribe to page scroll window event.
 addEventListener("scroll", (event) => {
-  const box = document.querySelector("#myspan");
+  this.toggleBtnHide();
+ 
   if (!isInViewport(box) && !isVisible) {
     isVisible = true;
 
@@ -31,12 +39,22 @@ addEventListener("scroll", (event) => {
     if (this.typed != null) {
       this.typed.destroy();
     }
-    
     this.typed = new Typed(mySpan, props);
   } else {
     isVisible = false;
   }
 });
+
+const btn = document.querySelector("#scroll-top");
+
+function toggleBtnHide(){
+  if(!isInViewport(box)){
+    btn.hidden = false;
+  }
+  else{
+    btn.hidden = true;
+  }
+}
 
 // Downlaod the resume.
 function downloadResume() {
